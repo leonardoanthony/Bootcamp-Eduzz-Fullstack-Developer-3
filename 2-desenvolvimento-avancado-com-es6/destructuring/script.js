d68e063ff3d9adab0d791b435caadd726a796f5c
leonardoanthony.dev <leonardoanthony.dev@gmail.com>
2022-02-08 20:53:11 -0300

    Destructuring Assignment

---
+++ 2-desenvolvimento-avancado-com-es6/destructuring/script.js
@@ -0,0 +1,18 @@
+//? Destructuring Assignment
+
+//! Modo antigo
+
+const arr = ['Apple', 'Banana', 'Orange'];
+
+const maca = arr[0];
+const banana = arr[1];
+const laranja = arr[2];
+
+//* Modo Atual
+
+const arr2 = ['Apple', 'Banana', 'Orange'];
+
+const [maca2, banana2, laranja2] = arr2;
+
+// o mesmo funciona com objetos e parametros de funções
+
