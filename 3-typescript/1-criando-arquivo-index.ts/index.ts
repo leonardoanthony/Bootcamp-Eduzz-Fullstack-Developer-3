5aebcfb535f61b8e8386ae04939c6add4da8745b
leonardoanthony.dev <leonardoanthony.dev@gmail.com>
2022-02-16 21:31:16 -0300

    Como funcionam os types

--- 3-typescript/1-criando-arquivo-index.ts/index.ts
+++ 3-typescript/1-criando-arquivo-index.ts/index.ts
@@ -10,13 +10,13 @@ soma(2,3);
 interface IAnimal {
     nome: string;
     tipo: 'terrestre' | 'aquatico';
-    executarRugido(alturaEmDecibeis: number): void;
+    // executarRugido(alturaEmDecibeis: number): void;
 }
 
 const animal: IAnimal = {
     nome: 'Elefante',
     tipo: 'terrestre',
-    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`),
+    // executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`),
 }
 
 //! animal.executarRugido('s');
@@ -30,6 +30,19 @@ const felino: IFelino = {
     nome: 'Leopardo',
     tipo: 'terrestre',
     visaoNoturna: true,
-    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`);
+    // executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`),
 }
 
+interface ICanino extends IAnimal{
+    porte: 'grande' | 'medio' | 'pequeno',
+}
+
+type IDomestico = IFelino | ICanino;
+// | seleciona características de um ou de outro
+// & seleciona características de ambos
+
+const domestico: IDomestico = {
+    nome: 'Cachorro',
+    tipo: 'terrestre',
+    porte: 'grande'
+}
