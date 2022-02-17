e89dbc29ad7ef14e829218e207035c7e6cbf36a0
leonardoanthony.dev <leonardoanthony.dev@gmail.com>
2022-02-16 21:09:38 -0300

    Como funcionam as interfaces

--- 3-typescript/1-criando-arquivo-index.ts/index.ts
+++ 3-typescript/1-criando-arquivo-index.ts/index.ts
@@ -1,1 +1,35 @@
-console.log('TypeScript');
+function soma (a: number, b: number){
+    return a + b;
+}
+
+soma(2,3);
+//! soma('a','b');
+
+//* Interfaces
+
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'aquatico';
+    executarRugido(alturaEmDecibeis: number): void;
+}
+
+const animal: IAnimal = {
+    nome: 'Elefante',
+    tipo: 'terrestre',
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`),
+}
+
+//! animal.executarRugido('s');
+//* animal.executarRugido(50);
+
+interface IFelino extends IAnimal{
+    visaoNoturna: boolean;
+}
+
+const felino: IFelino = {
+    nome: 'Leopardo',
+    tipo: 'terrestre',
+    visaoNoturna: true,
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`);
+}
+
